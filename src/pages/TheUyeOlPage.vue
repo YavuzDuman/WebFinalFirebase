d50b689a6eae5e969a1590f25321e4e2548e9c22
Yavuz Duman <dumanyavuz60@gmail.com>
2024-01-24 02:50:31 +0300

    Create TheUyeOlPage.vue

---
+++ src/pages/TheUyeOlPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div><TheHeader /></div>
+    <div style="margin-top: -450px;"><TheUyeOl /></div>
+    <div style="margin-top: 150px;"><TheFooter /></div>
+</template>
+  
+<script setup>
+  import TheHeader from 'components/TheHeader.vue';
+  import TheUyeOl from 'components/SingUp.vue'
+  import TheFooter from 'components/TheFooter.vue';
+ 
+</script>
