8dfdd4f37b948ee6e46fd8234f0f55bbc9b7cb94
Yavuz Duman <dumanyavuz60@gmail.com>
2024-01-24 02:50:12 +0300

    Create HomePage.vue

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div><TheHeader /></div>
+    <div style="margin-top: -500px;"><TheSlider /></div>
+    <div style=""><TheIcon /></div>
+    <div style="height: 1000px;"></div>
+    <div style="margin-top: 300px;"><TheFooter /></div>
+    
+
+
+</template>
+  
+<script setup>
+  import TheHeader from 'components/TheHeader.vue';
+  import TheSlider from 'components/SwiperSlide.vue';
+  import TheIcon from 'components/IconGeneral.vue';
+  import TheFooter from 'components/TheFooter.vue'; 
+</script>
