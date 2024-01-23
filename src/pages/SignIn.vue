11d754b9bf0868f93c3250a002b93f96feac6a85
Yavuz Duman <dumanyavuz60@gmail.com>
2024-01-24 02:50:26 +0300

    Create SignIn.vue

---
+++ src/pages/SignIn.vue
@@ -0,0 +1,15 @@
+<template>
+    <div><TheHeader /></div>
+    <div style="margin-top: -450px;"><TheUyeGiris /></div>
+    <div style="margin-top: 90px;"><TheFooter /></div>
+</template>
+  
+<script setup>
+  import TheHeader from 'components/TheHeader.vue';
+  import TheUyeGiris from 'components/TheUyeGiris.vue'
+  import TheFooter from 'components/TheFooter.vue';
+  
+  
+  
+  
+</script>
