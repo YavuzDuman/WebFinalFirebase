5a2cc3f3e11fde8b99366acd78f0061f000c8ddb
Yavuz Duman <dumanyavuz60@gmail.com>
2024-01-24 02:50:19 +0300

    Create SepetPage.vue

---
+++ src/pages/SepetPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div><TheHeader /></div>
+    <div style="margin-top: -450px;"><TheSepet /></div>
+    <div style="margin-top: 300px;"><TheFooter /></div>
+
+
+</template>
+  
+<script setup>
+  import TheHeader from 'components/TheHeader.vue';
+  
+  import TheSepet from 'components/TheSepet.vue';
+  import TheFooter from 'components/TheFooter.vue';
+  
+</script>
